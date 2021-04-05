85f6d7897965af6c2c66e4fde6dd255f97e1bcd5
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-04-05 17:26:06 -0400

    NOW we're making process!

--- templates/1resources/mobileMenu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("generalNavBar");
-  if (x.className === "menuBar") {
-    x.className += " responsive";
-  } else {
-    x.className = "menuBar";
-  }
-}
