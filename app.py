24c055762aeba85cc58552844ebde2f2a1157a0a
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-11-14 21:30:10 -0500

    LOGIN FUNCTIONALITY!!

--- app.py
+++ app.py
@@ -1,10 +1,69 @@
 from flask import *
+import os
+import flask
 from rethinkdb import r
 import rethinkdb
+import werkzeug
+
 r.set_loop_type('asyncio')
 app = Flask(__name__)
-
+app.secret_key = os.urandom(42)
 style_used = "style.css"
+
+# hi {{{
+import flask_login
+
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin): pass
+@login_manager.user_loader
+async def user_loader(uname):
+    conn = await r.connect("localhost",28015)
+    gotuser = await r.db("openclassroom").table("userSettings").filter(r.row["user_stuff"] == uname).run(conn)
+    async for gottuser in gotuser: pass #gets item from cursor
+    if gottuser == gotuser: return #This means that there were no items in the cursor, as there was nothing to `for`. I've tried for's ELSE statement but that always ran for some reason :thinking:
+    user = User()
+    user.id = uname
+    return user
+@login_manager.request_loader
+async def request_loader(request):
+    uname = request.form.get('uname')
+    conn = await r.connect("localhost",28015)
+    gotuser = await r.db("openclassroom").table("userSettings").filter(r.row["user_stuff"] == uname).run(conn)
+    async for gottuser in gotuser: pass #gets item from cursor
+    if gottuser == gotuser: return
+    user = User()
+    user.id = uname
+    return user
+
+@app.route('/login', methods=['GET','POST'])
+async def login():
+    if flask.request.method == 'GET':
+        return '''
+    <FORM ACTION='/login' METHOD='POST'>
+    <INPUT TYPE='TEXT' NAME='uname' ID='uname' PLACEHOLDER='username'/>
+    <INPUT TYPE='password' NAME='password' ID='password' PLACEHOLDER='password'/>
+    <input type='submit' name='submit'/>
+    </FORM>
+        '''
+    uname = flask.request.form['uname']
+    #print(werkzeug.security.generate_password_hash(flask.request.form['password'], salt_length=16))
+    if werkzeug.security.check_password_hash(await GetPass(uname), flask.request.form['password']):
+        user = User()
+        user.id = uname
+        flask_login.login_user(user)
+        return 'Logged in'
+    return 'Bad login'
+async def GetPass(uname):
+    conn = await r.connect("localhost",28015)
+    user = uname
+    userSettings = await r.db("openclassroom").table("userSettings").filter(r.row["user_stuff"] == user).run(conn)
+    async for DEETA in userSettings: pass
+    if DEETA == userSettings: return "" #relies on disallowing blank passwords
+    return DEETA['secret']
+# }}}
 
 @app.route("/")
 async def welcome():
@@ -38,7 +97,8 @@ async def dismiss(id):
     abort(418)
 @app.route("/RerunSetup")
 async def Setup():
-    return render_template("setup.html")
+    return "GO TO <a href='/RerunSetup/LoadDB'>LOADDB</a>"
+    #return render_template("setup.html")
 @app.route("/RerunSetup/LoadDB")
 async def CreateDatabase():
     conn = await r.connect("localhost", 28015)
@@ -52,6 +112,7 @@ async def CreateDatabase():
     await r.db("openclassroom").table_create("classes").run(conn)
     await r.db("openclassroom").table("userSettings").insert(
             {"user_stuff": "test",
+                "secret": werkzeug.security.generate_password_hash('pass', salt_length=16),
                 "mode": "dark"}
             ).run(conn)
     return "Hello World! In the future there'll be more shit to configure."
